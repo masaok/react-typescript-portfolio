cfa55c68441b30889920ffcf8ef8c1b3e08af557
Masao Kitamura <masaok@gmail.com>
2020-04-12 04:04:04 -0700

    Updates

---
+++ precache-manifest.f164780d2266a05ce099651008721d79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e83b64ca5fff4ff3118cdf87145a63",
+    "url": "/react-typescript-portfolio/index.html"
+  },
+  {
+    "revision": "02eaf161fc591da00a45",
+    "url": "/react-typescript-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4395e6aaffaf01f497c",
+    "url": "/react-typescript-portfolio/static/js/2.9a5af73f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-typescript-portfolio/static/js/2.9a5af73f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02eaf161fc591da00a45",
+    "url": "/react-typescript-portfolio/static/js/main.f6d0ede1.chunk.js"
+  },
+  {
+    "revision": "cba6aea9deebb9d09a75",
+    "url": "/react-typescript-portfolio/static/js/runtime-main.f6026368.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-typescript-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
